b7c34d59d9b6df6867311cbbdf6decf9fd9a6c24
LukasDb <lukas.dirnberger@tum.de>
2023-05-17 14:52:48 +0200

    removed old datatypes

--- cvde/data_types/__init__.py
+++
@@ -1,7 +0,0 @@
-from .data_type import DataType
-from .image import Image
-from .batch import Batch
-from .numerical import Numerical
-from .bounding_box import Bbox
-
-__all__=['Numerical', 'Batch', 'Image', 'DataType', 'Bbox']
--- cvde/data_types/numerical.py
+++
@@ -1,9 +0,0 @@
-from .data_type import DataType
-import numpy as np
-
-
-class Numerical(DataType):
-    def __init__(self, **kwargs) -> None:
-        """ Holds arbitrary numerical data (numpy, tensors, etc)"""
-        super().__init__(**kwargs)
-        
