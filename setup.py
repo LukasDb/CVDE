b52e1b045f3a460657018149ce1bda8d176f8adf
LukasDb <lukas.dirnberger@tum.de>
2023-06-06 15:35:55 +0200

    fixed installation of non-python files

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email="lukas.dirnberger@tum.de",
     license="todo",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=open("requirements.txt").readlines(),
     entry_points= {"console_scripts": ["cvde = cvde.__main__:run"]},
     classifiers=[
